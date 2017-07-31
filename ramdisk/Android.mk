deb3b04471b65c8ab9f1835126d92f4349e4e775
deadman96385 <seanhoyt963@gmail.com>
2017-07-31 01:07:28 -0500

    Add klimttd ramdisk changes
    Change-Id: Id8a3ac216c9d11022e609d08b094de6f7b3dcc07

--- ramdisk/Android.mk
+++ ramdisk/Android.mk
@@ -8,13 +8,6 @@ LOCAL_SRC_FILES         := fstab.universal5420
 LOCAL_MODULE_PATH       := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
-include $(CLEAR_VARS)
-LOCAL_MODULE		:= init.baseband.rc
-LOCAL_MODULE_TAGS	:= optional eng
-LOCAL_MODULE_CLASS	:= ETC
-LOCAL_SRC_FILES		:= init.baseband.rc
-LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
-include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE		:= init.target.rc
@@ -24,6 +17,7 @@ LOCAL_SRC_FILES		:= init.target.rc
 LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
+
 include $(CLEAR_VARS)
 LOCAL_MODULE		:= ueventd.universal5420.rc
 LOCAL_MODULE_TAGS	:= optional eng
@@ -31,3 +25,4 @@ LOCAL_MODULE_CLASS	:= ETC
 LOCAL_SRC_FILES		:= ueventd.universal5420.rc
 LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
+
